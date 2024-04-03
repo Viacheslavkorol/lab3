d842537873c47aa1fc0ead0e545fd53e6f810401
viacheslavkorol <viacheslav.korol11@gmai.com>
2024-04-03 21:01:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int a= 20;
+        int b= 7;
+        double c = (double) a/b;
+        System.out.println(c);
+
+
+
+
+
+        int x=35;
+        int y=x/10;
+        int z=x%10;
+        int p=y+z;
+        System.out.println(p);
+
+
+
+
+
+        double t=10.2;
+        int m=(int) (t+0.5);
+        System.out.println(m);
+    }
+}
